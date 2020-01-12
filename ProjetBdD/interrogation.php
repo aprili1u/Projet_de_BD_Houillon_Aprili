6712f0ee283197abb059c17a50d79490729950ee
aprili1u <44086493+aprili1u@users.noreply.github.com>
2020-01-12 23:21:58 +0100

    Add files via upload

--- ProjetBdD/interrogation.php
+++ ProjetBdD/interrogation.php
@@ -10,53 +10,52 @@
 <p>3 - Quel est le nombre de lits dans chaque service? &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question3"?>">Réponse</a></td>
 
-<p> 4 - Le titre et le nom de l’éditeur de tous les livres &nbsp;
+<p> 4 - Quel est le nombre de lits libres dans chaque salle du service de cardiologie au 04/07/2018?  &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question4"?>">Réponse</a></td>
 
-<p> 5 - Le titre, l’auteur et l’éditeur de tous les livres &nbsp;
+<p> 5 - Quels sont les patients qui n’ont jamais été traités par un cardiologue &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question5"?>">Réponse</a></td>
 
-<p> 6 - Le titre des livres dont l’auteur est "Isaac Asimov" &nbsp;
+<p> 6 - Quels sont les patients qui ont subi au moins un acte dans tous les services? &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question6"?>">Réponse</a></td>
 
-<p> 7 - Le nom des auteurs (sans doublons) publiés par l’éditeur "J’ai Lu" &nbsp;
+<p> 7 -  Quels sont les médecins, leur spécialité et le nom du patient, qui ont traités un patient qui a subit un acte dans tous les services de l’hopital? &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question7"?>">Réponse</a></td>
 
-<p> 8 - Le nombre de livres écrits par "Amélie Nothomb" &nbsp;
+<p> 8 - Quel sont les patients qui sont toujours restés plus de deux semaines lors de leurs hospitalisations?  &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question8"?>">Réponse</a></td>
 
-<p> 9 - Le nombre de livres publiés par Editeur &nbsp;
+<p> 9 - Quels sont les patients qui ont toujours été traités sans être hospitalisés? &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question9"?>">Réponse</a></td>
 
-<p> 10 - Les livres de science-fiction n’ayant pas été écrit par "Asimov Isaac" &nbsp;
+<p> 10 - Quels sont les services qui n’ont traités que des patients non hospitalisés?  &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question10"?>">Réponse</a></td>
 
-<p> 11 - Les auteurs publiés par les mêmes éditeurs &nbsp;
+<p> 11 - Quels sont les patients et le service, des patients qui n’ont eu un acte que dans un seul service? &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question11"?>">Réponse</a></td>
 
-<p> 12 - Les nouvelles de science-fiction écrites par "Asimov Isaac" et non éditées
-par "Gallimard" &nbsp;
+<p> 12 -  Quelles sont les services dont la majorité des patients ont été hospitalisés au moins 2 jours?  &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question12"?>">Réponse</a></td>
 
-<p> 13 - Les livres écrits par des auteurs décédés &nbsp;
+<p> 13 -  Quels sont les patients hospitalisés plus de trois jours qui ne sont pas à la mutelle MUT. &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question13"?>">Réponse</a></td>
 	
-<p> 14 - Les auteurs ayant écrits des livres de natures différentes &nbsp;
+<p> 14 - Quelestlenombremoyendepatients(diﬀérents)parmédecin(patient ayant subit un acte par le médecin)?  &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question14"?>">Réponse</a></td>
 
-<p> 15 - Les auteurs ayant écrits au moins deux livres &nbsp;
+<p> 15 - Quelle est la moyenne des actes par jour pour l’ensemble des medecins &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Question15"?>">Réponse</a></td>
 
-<p>  Requête supplémentaire 1 : Éditeurs publiant des auteurs russes &nbsp;
+<p>  Requête supplémentaire 1 : Les infirmiers habitant à Nancy qui travaillent en Cardiologie &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Requête1"?>">Réponse</a></td>
 
-<p>  Requête supplémentaire 2 : Nombre d’éditeurs différents ayant publié Isaac Asimov &nbsp;
+<p>  Requête supplémentaire 2 : Les patients qui ne sont restés que 1 jour &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Requête2"?>">Réponse</a></td>
 
-<p> Requête supplémentaire 3 : Liste des éditeurs publiant des auteurs non décédés &nbsp;
+<p> Requête supplémentaire 3 : Compter le nombre de salles de cancérologie &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Requête3"?>">Réponse</a></td>
 
-<p> Requête supplémentaire 4 : Liste des éditeurs et année des romans publiés entre 1940 et 1960 &nbsp;
+<p> Requête supplémentaire 4 : Liste les numeros des infirmiers travaillant en cardiologie &nbsp;
 <td><a href="retourRequete.php?id=<?php echo "Requête4"?>">Réponse</a></td>
 
 	
--- ProjetBdD/retourRequete.php
+++ ProjetBdD/retourRequete.php
@@ -36,17 +36,8 @@ while ($donnees = $reponse->fetch())
   
 $reponse->closeCursor();
 break;
-case "Question2" : 
-$request="SELECT M.Nummed FROM Service AS S JOIN Medecin AS M ON M.Nummed=S.Nummed WHERE M.specialite='Cancerologue'";
-$reponse = $bdd->query($request);
-echo "M.Nummed";
-while ($donnees = $reponse->fetch())
-{
-     echo $donnees[0] . '<br />';
-}
   
-$reponse->closeCursor();
-break;
+
 case "Question3" : 
 $request="SELECT NumServ,SUM(Nblits) FROM salle GROUP BY NumServ";
 $reponse = $bdd->query($request);
@@ -60,7 +51,199 @@ while ($donnees = $reponse->fetch())
 $reponse->closeCursor();
 break;
 
+case "Question4" : 
+     $request="SELECT P.Numservice,S.Nbtlit-P.Nbpat FROM 
+     (SELECT Numservice,COUNT(Numpat) AS Nbpat FROM Hospitalisation 
+     WHERE Numservice = (SELECT Numservice FROM Service WHERE Nom='Cardiologie') 
+     AND Dateentree < DATE '04/07/2018' AND Datesortie > DATE '04/07/2018' GROUP BY Numservice) 
+     AS P JOIN (SELECT Numservice,SUM(Nblits)AS Nbtlit FROM salle GROUP BY Numservice AS S) ON P.Numservice=S.Numservice";
+     $reponse = $bdd->query($request);
+     echo "P.Numservice,S.Nbtlit-P.Nbpat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . ' , ' ;
+          echo $donnees[1] .'<br />';
+     }
+$reponse->closeCursor();
+break;
 
+case "Question5" : 
+     $request="SELECT Numpat FROM Hospitalisation WHERE NOT Numservice = (SELECT Numservice FROM Service WHERE Nom='Cardiologie')";
+     $reponse = $bdd->query($request);
+     echo "Numpat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+case "Question6" : 
+     $request="SELECT DISTINCT Numpat FROM Acte AS ac1 WHERE NOT EXISTS (SELECT * FROM Service AS ser 
+     WHERE NOT EXISTS (SELECT * FROM Acte AS ac WHERE ac.Numpat=ac1.Numpat AND ser.Numservice=ac.Numservice))";
+     $reponse = $bdd->query($request);
+     echo "Numpat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question7" : 
+     $request="SELECT Med.Nummed,Med.specialite,Ac.Numpat 
+     FROM Medecin AS Med JOIN Acte AS Ac ON Med.Nummed = Ac.Nummed WHERE Ac.Numpat = 
+     (SELECT DISTINCT Numpat FROM Acte AS ac1 WHERE NOT EXISTS (SELECT * FROM Service AS ser WHERE NOT EXISTS 
+     (SELECT * FROM Acte AS ac WHERE ac.Numpat=ac1.Numpat AND ser.Numservice=ac.Numservice)))";
+     $reponse = $bdd->query($request);
+     echo "Med.Nummed,Med.specialite,Ac.Numpat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . ',';
+          echo $donnees[1] . ',';
+          echo $donnees[2] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question8" : 
+     $request="SELECT Numpat FROM Hospitalisation WHERE DATEDIFF(Datesortie,Dateentree)>=14";
+     $reponse = $bdd->query($request);
+     echo "Numpat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question9" : 
+     $request="SELECT DISTINCT Numpat FROM Acte WHERE Numpat NOT IN (SELECT A.Numpat FROM Acte AS A JOIN hospitalisation AS H ON A.Numpat=H.Numpat AND A.Numservice=H.Numservice AND A.Dateacte!=H.Dateentree)";
+     $reponse = $bdd->query($request);
+     echo "Numpat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question10" : 
+     $request="SELECT DISTINCT A.Numpat FROM Acte AS A WHERE A.Numpat NOT IN (SELECT H.Numpat FROM Hospitalisation AS H)";
+     $reponse = $bdd->query($request);
+     echo "A.Numpat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question11" : 
+     $request="SELECT A.Numpat,A.Numservice FROM Acte AS A JOIN (SELECT COUNT(Numpat) AS cnt,Numpat FROM Acte GROUP BY Numpat) AS B ON A.Numpat=B.Numpat WHERE B.cnt=1";
+     $reponse = $bdd->query($request);
+     echo "A.Numpat,A.NumService";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . ',';
+          echo $donnees[1] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question12" : 
+     $request="SELECT A.Numservice FROM (SELECT COUNT(H.Numpat) AS C, H.Numservice FROM Hospitalisation 
+     AS H WHERE DATEDIFF(Datesortie,Dateentree)>=2 GROUP BY H.Numservice) AS A JOIN (SELECT COUNT(H1.Numpat)/2 
+     AS MOITIE,H1.Numservice FROM Hospitalisation AS H1 GROUP BY H1.Numservice) 
+     AS B ON A.Numservice=B.Numservice WHERE A.C >= B.MOITIE";
+     $reponse = $bdd->query($request);
+     echo "A.Numservice";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question13" : 
+     $request="SELECT H.Numpat FROM Hospitalisation AS H JOIN (SELECT Mutuelle,Numpat FROM Patient) AS P ON H.Numpat=P.Numpat 
+     WHERE DATEDIFF(H.Datesortie,H.Dateentree)>=3 AND P.Mutuelle!='MUT'";
+     $reponse = $bdd->query($request);
+     echo "A.Numpat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question14" : 
+     $request="SELECT COUNT(DISTINCT Numpat),Nummed FROM Acte GROUP BY Nummed";
+     $reponse = $bdd->query($request);
+     echo "COUNT(DISTINCT Numpat),Nummed";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . ',';
+          echo $donnees[1] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Question15" : 
+     $request="SELECT COUNT(Numpat),Dateacte FROM Acte GROUP BY Dateacte";
+     $reponse = $bdd->query($request);
+     echo "COUNT(Numpat),Dateacte";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] . ',';
+          echo $donnees[1] . '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Requête1" : 
+     $request="SELECT nom from infirmier as inf join salle as sa on inf.NumInf=sa.NumInf WHERE
+      NumServ = (Select distinct Numserv from salle join service as se on se.NumService=NumServ Where se.nom='Cardiologie') and adresse='Nancy'";
+     $reponse = $bdd->query($request);
+     echo "nom";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] .  '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Requête2" : 
+     $request="SELECT NumPat FROM Patient as pa join hospitalisation as hos on pa.NumPat=hos.NumPat where datediff(hos.datesortie,hos.dateentree)=1 ";
+     $reponse = $bdd->query($request);
+     echo "NumPat";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] .  '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Requête3" : 
+     $request="SELECT count( distinct NumSalle) from salle as sa join service as se on sa.numserv=numserv where (se.nom='cancerologie')";
+     $reponse = $bdd->query($request);
+     echo "count( distinct NumSalle)";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] .  '<br />';
+     }
+$reponse->closeCursor();
+break;
+
+case "Requête4" : 
+     $request="SELECT NumInf from salle where numServ=1";
+     $reponse = $bdd->query($request);
+     echo "count( distinct NumSalle)";
+     while ($donnees = $reponse->fetch())
+     {
+          echo $donnees[0] .  '<br />';
+     }
+$reponse->closeCursor();
+break;
 }
 ?>
 
